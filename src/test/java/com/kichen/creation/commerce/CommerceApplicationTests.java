9c0b9fe2fdea100bd55dbe9b211b4cf9ecbe06a7
Jung Hwan Park <jhpark904@gmail.com>
2023-12-26 19:42:54 -0500

    첫 PR
    - 스프링 부트 프로젝트 생성
    - github action workflow file 생성, build와 test action 추가

---
+++ src/test/java/com/kichen/creation/commerce/CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kichen.creation.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9cc90e75f251ac3fac267162b1038de333fbda35
Jung Hwan Park <jhpark904@gmail.com>
2023-12-30 20:41:39 -0500

    엔티티 객체 더하기
    - lombok과 spring data jpa dependency 추가
    - Order, OrderLine, Product entity 객체 추가

--- src/main/java/com/kichen/creation/commerce/CommerceApplication.java
+++ src/main/java/com/kichen/creation/commerce/CommerceApplication.java
@@ -2,8 +2,10 @@ package com.kichen.creation.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/config.properties") // TODO: 애노테이션 제거 가능하면 하기
 public class CommerceApplication {
 
 	public static void main(String[] args) {
